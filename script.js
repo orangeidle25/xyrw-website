694be69e85f1a25864f12b136af48cc2b4f7f27b
xyrw <bungees-cents.0s@icloud.com>
2025-02-14 14:48:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function rlink() {
     alert("Would you like to go to the Roblox group?")
 }
+function rlink1() {
+    alert("Would you like to go to the Roblox group?")
+}
+function rlink2() {
+    alert("Would you like to go to the Roblox group?")
+}
+function rlink3() {
+    alert("Would you like to go to the Roblox group?")
+}
+function rlink4() {
+    alert("Would you like to go to the Roblox group?")
+}
+function rlink5() {
+    alert("Would you like to go to the Roblox group?")
+}
