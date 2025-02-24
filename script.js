28aa3ef0d501754a560aeae31cd98c14b99bbd74
xyrw <bungees-cents.0s@icloud.com>
2025-02-23 21:21:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-function rlink() {
-    alert("Would you like to go to the Roblox group?")
-}
-function rlink1() {
-    alert("Would you like to go to the Roblox group?")
-}
-function rlink2() {
-    alert("Would you like to go to the Roblox group?")
-}
-function rlink3() {
-    alert("Would you like to go to the Roblox group?")
-}
-function rlink4() {
-    alert("Would you like to go to the Roblox group?")
-}
-function rlink5() {
-    alert("Would you like to go to the Roblox group?")
-}
+const consentBox = 
+    document.getElementById("consentBox");
+const acceptBtn = 
+    document.querySelector(".consentButton");
+const rejectBtn = 
+    document.querySelector(".rejectButton");
+
+acceptBtn.onclick = () => {
+    document.cookie = "CookieBy=GeeksForGeeks; max-age="
+        + 60 * 60 * 24;
+    if (document.cookie) {
+        consentBox.classList.add("hide");
+    } else {
+        alert
+            ("Cookie can't be set! Please"+
+              " unblock this site from the cookie"+
+              " setting of your browser.");
+    }
+};
+
+rejectBtn.onclick = () => {
+    alert(
+        "Cookies rejected. Some functionality may be limited.");
+    consentBox.classList.add("hide");
+};
+
+let checkCookie = 
+    document.cookie.indexOf("CookieBy=GeeksForGeeks");
+checkCookie !== -1 ? consentBox.classList.add("hide") :
+    consentBox.classList.remove("hide");
